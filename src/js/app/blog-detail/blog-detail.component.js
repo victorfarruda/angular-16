4c2b4b6666e19eeebc87c007576093546a49f0de
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-05-18 12:54:32 -0300

    aula 21

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,34 +3,19 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($http, $location, $routeParams, $scope){
-            $http.get('/json/posts.json', {}).then(successCallback, errorCallback)
-
-            function successCallback(response, status, config, statusText){
+        controller: function(Post, $location, $routeParams, $scope){
+            $scope.posts = Post.query()
+            Post.query(function(data){
                 $scope.notFound = true
-                var blogItems = response.data
-                $scope.posts = blogItems
-                angular.forEach(blogItems, function(post){
+                angular.forEach(data, function(post){
                     if (post.id == $routeParams.id){
                         $scope.post = post
                         $scope.notFound = false
-                        console.log(post)
                     }
                 })
-            }
-            function errorCallback(response, status, config, statusText){
-                $scope.notFound = true
-                console.log(response)
-            }
-//            angular.forEach(blogItems, function(post){
-//                if (post.id == $routeParams.id){
-//                    $scope.post = post
-//                    $scope.notFound = false
-//                }
-//            })
-
-            if ($scope.notFound){
-                $location.path("/404")
-            }
+                if ($scope.notFound){
+                    $location.path("/404")
+                }
+            })
         }
     });
