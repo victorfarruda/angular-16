4e47507a148883e84def2112a65a7df28d00b2cc
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-05-18 08:05:56 -0300

    aula 19

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,7 +3,7 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($routeParams, $scope){
+        controller: function($location, $routeParams, $scope){
             var blogItems = [
                 {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
                 {title: "Title", id: 2, description: "This is a book"},
@@ -11,7 +11,17 @@ angular.module('blogDetail').
                 {title: "Lite", id: 4, description: "This is a book"},
             ]
 
-            $scope.items = blogItems;
+            $scope.notFound = true
+            angular.forEach(blogItems, function(post){
+                if (post.id == $routeParams.id){
+                    $scope.post = post
+                    $scope.notFound = false
+                }
+            })
+
+            if ($scope.notFound){
+                $location.path("/404")
+            }
             $scope.title = "Blog " + $routeParams.id
         }
     });
