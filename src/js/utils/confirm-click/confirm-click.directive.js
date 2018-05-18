a4a68755c0a7920362e959996ccf7bc909689809
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-05-18 16:39:35 -0300

    aula 22

---
+++ src/js/utils/confirm-click/confirm-click.directive.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.
+    module('confirmClick').
+        directive('confirmClick', function(){
+            return {
+                restrict: "A",
+                link: function(scope, element, attr){
+                    var msg = attr.confirmClick || "Are you sure?"
+                    var clickAction = attr.confirmedClick
+                    element.bind('click', function(event){
+                        if (window.confirm(msg)){
+                            scope.$eval(clickAction)
+                        }
+                    })
+                }
+            }
+        });
