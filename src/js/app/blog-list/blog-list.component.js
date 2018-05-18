9b037aa28e3f368f5d48be45f2380e940bb15ee5
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-05-18 17:43:45 -0300

    aula 23

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,7 +3,12 @@
 angular.module('blogList').
     component('blogList', {
         templateUrl: '/templates/blog-list.html',
-        controller: function(Post, $scope){
+        controller: function(Post, $location, $rootScope, $scope){
+            $scope.goToItem =  function (item){
+                $rootScope.$apply(function(){
+                    $location.path("/blog/" + item.id)
+                })
+            }
             $scope.items = Post.query();
         }
     });
